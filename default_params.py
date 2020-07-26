83f3eb11f5155e788c38ef0dfcae39b42162c457
taufeeque9 <9taufeeque9@gmail.com>
2020-07-26 19:21:17 +0530

    Removed gf feature

--- default_params.py
+++ default_params.py
@@ -1,9 +1,9 @@
-DEFAULT_CONSEC_FRAMES = 8
+DEFAULT_CONSEC_FRAMES = 36
 MIN_THRESH = 0.5
 TAKE_AVG_VALS_GF = False
 HIST_THRESH = 0.2
 
 HEAD_THRESHOLD = 1e-5
 
-FEATURE_SCALAR = {"ratio_bbox":1,"angle_vertical":1,"re":1,"gf":1,"ratio_derivative":1,"log_angle":1}
-FEATURE_LIST = ["ratio_bbox","angle_vertical","re","gf","ratio_derivative","log_angle"]
+FEATURE_SCALAR = {"ratio_bbox": 1, "angle_vertical": 1, "re": 1, "ratio_derivative": 1, "log_angle": 1}
+FEATURE_LIST = ["ratio_bbox", "angle_vertical", "re", "ratio_derivative", "log_angle"]
